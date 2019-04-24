008efad96e20017e4d7dfe7a3f05676d8b8c5e55
蘇靖軒 <music1353@gmail.com>
2019-04-24 17:24:09 +0800

    db backup system

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from datetime import datetime
@@ -63,9 +64,68 @@ def init_users_daily_games():
             users_daily_games_collect.insert_one(doc)
             print('init', user['account'], 'complete at', time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
+def backup():
+    nowTime = datetime.now().strftime("%Y-%m-%d") # 今天日期
+
+    # 創建資料夾
+    try:
+        mkdir_cmd = 'mkdir ./backup/' + nowTime
+        os.system(mkdir_cmd)
+        print('創建', nowTime, '資料夾成功！')
+    except Exception as err:
+        print('創建', nowTime, '資料夾失敗！')
+        print('err:', err)
+
+    # 備份每個collection的指令
+    users_cmd = {
+        'name': 'users',
+        'cmd': 'mongoexport --db cgu_db --collection users --out ./backup/'+nowTime+'/users.json'
+    }
+
+    admins_cmd = {
+        'name': 'admins',
+        'cmd': 'mongoexport --db cgu_db --collection admins --out ./backup/'+nowTime+'/admins.json'
+    }
+
+    parents_cmd = {
+        'name': 'parents',
+        'cmd': 'mongoexport --db cgu_db --collection parents --out ./backup/'+nowTime+'/parents.json'
+    }
+
+    comp_users_game_count_cmd = {
+        'name': 'comp_users_game_count',
+        'cmd': 'mongoexport --db cgu_db --collection comp_users_game_count --out ./backup/'+nowTime+'/comp_users_game_count.json'
+    }
+
+    users_daily_games_cmd = {
+        'name': 'users_daily_games',
+        'cmd': 'mongoexport --db cgu_db --collection users_daily_games --out ./backup/'+nowTime+'/users_daily_games.json'
+    }
+
+    users_games_level_cmd = {
+        'name': 'users_games_level',
+        'cmd': 'mongoexport --db cgu_db --collection users_games_level --out ./backup/'+nowTime+'/users_games_level.json'
+    }
+
+    users_games_records_cmd = {
+        'name': 'users_games_records',
+        'cmd': 'mongoexport --db cgu_db --collection users_games_records --out ./backup/'+nowTime+'/users_games_records.json'
+    }
+    
+    # 執行備份
+    cmd_list = [users_cmd, admins_cmd, parents_cmd, comp_users_game_count_cmd, users_daily_games_cmd, users_games_level_cmd, users_games_records_cmd]
+    for cmd in cmd_list:
+        try:
+            os.system(cmd['cmd'])
+            print('backup', cmd['name'], 'collection complete at', time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+        except Exception as err:
+            print('backup', cmd['name'], 'error ! at', time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+            print('err:', err)
+
 
 scheduler = BackgroundScheduler()
 # cheduler.add_job(func=init_users_daily_games, trigger="interval", seconds=10)
+scheduler.add_job(func=backup, trigger="cron", hour=17, minute=20, second=1)
 scheduler.add_job(func=init_users_daily_games, trigger="cron", hour=18, minute=16, second=1)
 scheduler.start()
 
@@ -73,5 +133,5 @@ scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
-    # app.run(use_reloader=False)
-    app.run(debug=True) # 會執行兩次
+    app.run(use_reloader=False)
+    # app.run(debug=True) # 會執行兩次
