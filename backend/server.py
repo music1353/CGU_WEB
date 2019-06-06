8962379e470d5337391be48d268743c1988fb2f0
music1353 <music1353@gmail.com>
2019-06-06 13:03:08 +0800

    use waitress WSGI server in backend

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,20 @@
+# use waitress WSGI server
+
+from waitress import serve
+from app import app
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
-from app import app
 from app.modules.backup_engine.drive import drive
 from app.modules.schedule_func import func
 
 def cloud_backup():
-    # google drive 備份
     googledrive = drive()
     googledrive.backup()
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=func.local_backup, trigger="cron", hour=12, minute=58, second=1)
-scheduler.add_job(func=cloud_backup, trigger="cron", hour=12, minute=59, second=30)
+scheduler.add_job(func=func.local_backup, trigger="cron", hour=12, minute=57, second=1)
+scheduler.add_job(func=cloud_backup, trigger="cron", hour=12, minute=57, second=5)
 scheduler.add_job(func=func.update_week, trigger="cron", hour=0, minute=0, second=1)
 # scheduler.add_job(func=update_week, trigger="cron", hour=15, minute=49, second=1)
 scheduler.add_job(func=func.init_mission, trigger="cron", hour=0, minute=0, second=2)
@@ -23,5 +25,5 @@ scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
-    # app.run(use_reloader=False)
-    app.run(debug=True) # 會執行兩次
+    serve(app, host='0.0.0.0', port=5000)
+
